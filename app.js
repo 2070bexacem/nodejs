d09e872f1ba99b7f74729db1c1c8b470858c42c7
manutdmohit <saudmohit@gmail.com>
2021-09-27 22:38:11 +0545

    added nodemon

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const lodash = require('lodash');
 
-const items = [1, 2, [3, 4, [5, 6]]];
+const items = [1, 2, [3, 4, [5, 6]],[7,8,[9,10]]];
 const newItems = lodash.flattenDeep(items);
 console.log(newItems);
+
+console.log("hello world");
