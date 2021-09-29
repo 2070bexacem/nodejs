59c22f4e2511944735554dab05649b7d04061f1d
manutdmohit <saudmohit@gmail.com>
2021-09-29 12:18:41 +0545

    added event loop

---
+++ eventloop/app.js
@@ -0,0 +1,13 @@
+// console.log('Start');
+
+// setTimeout(() => {
+//   console.log('Middle');
+// }, 0);
+
+// console.log('End');
+
+setInterval(() => {
+  console.log('Running');
+}, 0);
+
+console.log('I will run first');
